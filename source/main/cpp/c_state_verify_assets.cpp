359fc0caaef8436e3f35085a75057cb32c162c3d
jurgen.kluft <jurgen.kluft@gmail.com>
2026-09-19 20:19:01 +0800

    * rgui
    * verify assets; wip

--- source/main/cpp/c_state_verify_assets.cpp
+++ source/main/cpp/c_state_verify_assets.cpp
@@ -4,16 +4,15 @@
 
 #include "lib_guition/c_sdcard.h"
 
-
 #include "main/c_app_data.h"
 
 namespace ncore
-{ 
+{
     void state_handle_assets(fsm_state_data_t* state_data, app_data_t* app_data, u64 now_ms)
     {
         if (state_data->m_current_state == 0)
         {
-            nlog::log_info("VERIFY", "Begin");
+            nlog::log_info("VERIFY", "Begin verifying assets");
 
             // Verify sprite, font and palette packs
 
@@ -23,14 +22,57 @@ namespace ncore
             // them to the appropriate storage (e.g., SD card and/or LittleFS)
             if (app_data->m_sd_card_available)
             {
-                if (app_data-)
-
+                if (app_data->m_sprite_pack_size > 0 && app_data->m_sprite_pack_source != EDATA_SOURCE_SD_CARD)
+                {
+                    nlcd::sdcard_write_file("sprite_pack.bin", (u8*)app_data->m_sprite_pack, app_data->m_sprite_pack_size);
+                }
+                if (app_data->m_font_pack_size > 0 && app_data->m_font_pack_source != EDATA_SOURCE_SD_CARD)
+                {
+                    nlcd::sdcard_write_file("font_pack.bin", (u8*)app_data->m_font_pack, app_data->m_font_pack_size);
+                }
+                if (app_data->m_palette_pack_size > 0 && app_data->m_palette_pack_source != EDATA_SOURCE_SD_CARD)
+                {
+                    nlcd::sdcard_write_file("palette_pack.bin", (u8*)app_data->m_palette_pack, app_data->m_palette_pack_size);
+                }
+                if (app_data->m_script_binary_size > 0 && app_data->m_script_binary_source != EDATA_SOURCE_SD_CARD)
+                {
+                    nlcd::sdcard_write_file("script.bin", (u8*)app_data->m_script_binary, app_data->m_script_binary_size);
+                }
+                if (app_data->m_house_meta_size > 0 && app_data->m_house_meta_source != EDATA_SOURCE_SD_CARD)
+                {
+                    nlcd::sdcard_write_file("house.bin", (u8*)app_data->m_house_meta, app_data->m_house_meta_size);
+                }
             }
 
-            // Leave
+            if (app_data->m_little_fs_available)
+            {
+                if (app_data->m_sprite_pack_size > 0 && app_data->m_sprite_pack_source != EDATA_SOURCE_LITTLE_FS)
+                {
+                    nlittlefs::file_save("sprite_pack.bin", (u8*)app_data->m_sprite_pack, app_data->m_sprite_pack_size);
+                }
+                if (app_data->m_font_pack_size > 0 && app_data->m_font_pack_source != EDATA_SOURCE_LITTLE_FS)
+                {
+                    nlittlefs::file_save("font_pack.bin", (u8*)app_data->m_font_pack, app_data->m_font_pack_size);
+                }
+                if (app_data->m_palette_pack_size > 0 && app_data->m_palette_pack_source != EDATA_SOURCE_LITTLE_FS)
+                {
+                    nlittlefs::file_save("palette_pack.bin", (u8*)app_data->m_palette_pack, app_data->m_palette_pack_size);
+                }
+                if (app_data->m_script_binary_size > 0 && app_data->m_script_binary_source != EDATA_SOURCE_LITTLE_FS)
+                {
+                    nlittlefs::file_save("script.bin", (u8*)app_data->m_script_binary, app_data->m_script_binary_size);
+                }
+                if (app_data->m_house_meta_size > 0 && app_data->m_house_meta_source != EDATA_SOURCE_LITTLE_FS)
+                {
+                    nlittlefs::file_save("house.bin", (u8*)app_data->m_house_meta, app_data->m_house_meta_size);
+                }
+            }
+
+            // Next state 
             to_state_next(state_data);
-            nlog::log_info("VERIFY", "Ok");
-        }        
+
+            nlog::log_info("VERIFY", "Assets are Ok");
+        }
     }
 
 }  // namespace ncore
